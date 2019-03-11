4ffb1ba83fe6aa102e07b4ebfb91b1197bd0e0a7
Romain Testard <40339476+TestardR@users.noreply.github.com>
2019-03-11 19:24:24 +0100

    Delete database.js

--- config/database.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    mongoURI: 'mongodb://admin:Under3128@ds113003.mlab.com:13003/vidjotprod'
-  };
-} else {
-  module.exports = { mongoURI: 'mongodb://localhost/vidjot-dev' };
-}
